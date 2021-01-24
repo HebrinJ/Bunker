f6e13e5553e03743626282759102d607e623e51e
Hebrin <ashram28@gmail.com>
2021-01-24 17:22:55 +0300

    Добавлен шейдер выделения спрайтов. Создан скрипт замены материалов для работы выделения комнат.

---
+++ Assets/Scripts/Outline.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class Outline : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    public Material defaultMaterial, outlineMaterial;
+
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+    private void OnMouseEnter()
+    {
+        sprite.material = outlineMaterial;
+    }
+
+    private void OnMouseExit()
+    {
+        sprite.material = defaultMaterial;
+    }
+}
