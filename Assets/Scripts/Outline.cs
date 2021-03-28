5e085915d475b739638e81e3982913f8de712ac4
HebrinJ <70446296+HebrinJ@users.noreply.github.com>
2021-03-28 11:57:04 +0300

    Последняя версия перед ребилдом

--- Assets/Scripts/Outline.cs
+++ Assets/Scripts/Outline.cs
@@ -6,12 +6,13 @@ public class Outline : MonoBehaviour
     private SpriteRenderer sprite;
     public Material defaultMaterial, outlineMaterial;
 
+
     private void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
     }
     private void OnMouseEnter()
-    {
+    {   
         sprite.material = outlineMaterial;
     }
 
